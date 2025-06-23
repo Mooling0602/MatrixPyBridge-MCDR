c6efbab1ee14472126027c27b77adbcd99c39791
Mooling0602 <clemooling@outlook.com>
2025-06-23 14:38:39 +0800

    fix(src/mcdr.entry): change loading and unloading messages

--- matrix_py_bridge/mcdr/entry.py
+++ matrix_py_bridge/mcdr/entry.py
@@ -2,8 +2,8 @@ from mcdreforged.api.all import PluginServerInterface
 
 
 def on_load(server: PluginServerInterface, _prev_module):
-    server.logger.info("Loading MatrixPyBridge...")
+    server.logger.info("Loading MatrixPyBridge in MCDR...")
 
 
 def on_unload(server: PluginServerInterface):
-    server.logger.info("Unloading MatrixPyBridge...")
+    server.logger.info("Unloading MatrixPyBridge in MCDR...")
