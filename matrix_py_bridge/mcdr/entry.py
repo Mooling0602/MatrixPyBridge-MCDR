8dc99eb80a1e69e699d12bd294e8d3e26b77ce7d
Mooling0602 <clemooling@outlook.com>
2025-06-23 14:44:33 +0800

    feat(src/mcdr.entry): add a new working message when server startup

--- matrix_py_bridge/mcdr/entry.py
+++ matrix_py_bridge/mcdr/entry.py
@@ -5,5 +5,9 @@ def on_load(server: PluginServerInterface, _prev_module):
     server.logger.info("Loading MatrixPyBridge in MCDR...")
 
 
+def on_server_startup(server: PluginServerInterface):
+    server.logger.info("Game message listener will start!")
+
+
 def on_unload(server: PluginServerInterface):
     server.logger.info("Unloading MatrixPyBridge in MCDR...")
